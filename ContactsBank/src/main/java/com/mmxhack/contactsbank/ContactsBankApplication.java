8c6385ad008cd8726c2df3f7b0ec6852f3627faf
shyam <shyaamsathyanathan@gmail.com>
2015-06-27 14:51:46 +0530

    Initial commit

---
+++ ContactsBank/src/main/java/com/mmxhack/contactsbank/ContactsBankApplication.java
@@ -0,0 +1,12 @@
+package com.mmxhack.contactsbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContactsBankApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ContactsBankApplication.class, args);
+    }
+}
